518784cb0825a6072c1e33ef01c5af3f50e71280
denich <denis.feshchenko@gmail.com>
2015-06-22 03:17:58 +0300

    scaffolding: added base markup

---
+++ src/app/app.controller.spec.js
@@ -0,0 +1,19 @@
+(function () {
+	'use strict';
+
+	describe('AppController', function () {
+		var vm;
+
+		beforeEach(module('app'));
+
+		beforeEach(function () {
+
+		});
+
+		function execController() {
+			vm = $controller('AppController', {});
+		}
+
+
+	});
+})();
