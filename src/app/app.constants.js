5f4a8a872f2a7f46692a2fe297c3ee20c7ad1794
denich <denis.feshchenko@gmail.com>
2015-07-11 21:40:00 +0300

    scaffolding: added core module constants

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -2,7 +2,6 @@
 	'use strict';
 
 	angular
-		.module('app')
-		.constant('NAV_MENU_ID', 'nav-menu');
-	
+		.module('app');
+
 })();
---
+++ src/app/components/core.constants.js
@@ -0,0 +1,7 @@
+(function () {
+	'use strict';
+
+	angular
+		.module('app.core')
+		.constant('NAV_MENU_ID', 'nav-menu');
+})();
